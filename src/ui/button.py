4e0e15d667e257328da0604ebc813cb510de1097
Gallon Zhou <gallonzhou04@gmail.com>
2024-02-08 23:07:26 +1300

    docs: Added Docstrings To Button

--- src/ui/button.py
+++ src/ui/button.py
@@ -1,19 +1,52 @@
+"""This module allows for the creation of a button object."""
+
+from typing import TYPE_CHECKING, Optional
+
 import pygame
 
 from src.ui.element import Element
 
+if TYPE_CHECKING:
+    from src.app.app import App
+
 
 class Button(Element):
-    def __init__(self, app, **kwargs):
+    """This class initializes the button component. The button
+    component controls certain UI components, or even application
+    functionalities."""
+
+    def __init__(self, app: "App", **kwargs) -> None:
+        """Initializes the button object.
+
+        Args:
+            app (App): Singleton instance of app.
+            **kwargs: Keyworded, variable-length argument dictionary.
+        """
+
         super().__init__(app, **kwargs)
 
-    def update(self, event):
+    def update(self, event: Optional[pygame.Event]) -> None:
+        """Updates the button component.
+
+        Args:
+            event (Optional[pygame.Event]): The current pygame event.
+        """
+
         if event.type == pygame.MOUSEBUTTONDOWN:
             self.on_click(event)
 
         self.on_slide()
 
-    def on_click(self, event):
+    def on_click(self, event: Optional[pygame.Event]) -> None:
+        """Handles the clicking of the button component.
+
+        Args:
+            event (Optional[pygame.Event]): The current pygame event.
+
+        Returns:
+            None: Nothing is returned.
+        """
+
         if self.moveable.is_moving:
             return
 
@@ -22,7 +55,10 @@ class Button(Element):
 
         self.on_click_type()
 
-    def on_click_type(self):
+    def on_click_type(self) -> None:
+        """Handles the clicking of the button. This method handles
+        selection buttons."""
+
         match self.type:
             case "default":
                 pass
