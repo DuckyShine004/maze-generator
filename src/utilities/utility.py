288cf603b3bbd0db50377ca6d5d05728ec081f15
Gallon Zhou <gallonzhou04@gmail.com>
2024-02-08 21:14:14 +1300

    feat: Added Documentation For Utility Module

--- src/utilities/utility.py
+++ src/utilities/utility.py
@@ -1,3 +1,7 @@
+"""The module contains methods that are beneficial for every other module."""
+
+from typing import Dict, Tuple, Any
+
 import os
 import json
 import random
@@ -7,25 +11,51 @@ from src.constants.constants import MAZE_WIDTH, MAZE_HEIGHT
 
 
 class Utility:
+    """The Utility class contains useful methods which other modules might need."""
+
     @staticmethod
-    def get_random_position():
+    def get_random_position() -> Tuple[int, int]:
+        """Returns a random cell position.
+
+        Returns:
+            Tuple[int, int]: random cell position.
+        """
+
         x = random.randint(0, MAZE_WIDTH - 1)
         y = random.randint(0, MAZE_HEIGHT - 1)
 
         return (x, y)
 
     @staticmethod
-    def get_json_data(path):
+    def get_json_data(path: str) -> Any:
+        """Returns the JSON data.
+
+        Args:
+            path (str): The path to the JSON file.
+
+        Returns:
+            Any: JSON data.
+        """
+
         directory_path = os.path.dirname(os.path.realpath(__file__))
         absolute_path = os.path.join(directory_path, path)
 
-        with open(absolute_path, "r") as file:
+        with open(absolute_path, "r", encoding="utf-8") as file:
             data = json.load(file)
 
         return data
 
     @staticmethod
-    def get_images(paths):
+    def get_images(paths: Dict[str, str]) -> Dict[str, pygame.Surface]:
+        """Returns a list of pygame loaded images.
+
+        Args:
+            paths (Dict[str, str]): A list of paths.
+
+        Returns:
+            Dict[str, pygame.Surface]: List of pygame images.
+        """
+
         images = {}
 
         for key, path in paths.items():
@@ -34,7 +64,18 @@ class Utility:
         return images
 
     @staticmethod
-    def clamp(value, _min, _max):
+    def clamp(value: float, _min: float, _max: float) -> float:
+        """Clamps the input value between _min and _max.
+
+        Args:
+            value (float): The value to be clamped.
+            _min (float): The lower bound.
+            _max (float): The upper bound.
+
+        Returns:
+            float: The clamped value.
+        """
+
         if value < _min:
             return _min
 
