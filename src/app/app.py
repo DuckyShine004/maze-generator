ebb57fde4e147be78c6b659e664ae1fba03055c0
Gallon Zhou <gallonzhou04@gmail.com>
2024-02-08 21:47:44 +1300

    docs: Added Documentation For App Module

--- src/app/app.py
+++ src/app/app.py
@@ -1,22 +1,38 @@
+"""This module defines the lifecycle of the application."""
+
+from typing import Optional
+
 import pygame
 
 from src.ui.ui import UI
 from src.models.graph.graph import Graph
 
-from src.constants.constants import SURFACE_COLOR, WIDTH, HEIGHT, MENU
+from src.constants.constants import SURFACE_COLOR, FPS, WIDTH, HEIGHT, MENU
 
 
 class App:
-    def __init__(self):
-        self.surface = pygame.display.set_mode((WIDTH, HEIGHT))
+    """The module for keeping the application running, or the module which defines
+    the lifecycle of the application.
 
-        self.graph = Graph()
-        self.ui = UI(self, MENU)
+    Attributes:
+        event (pygame.Event): The current event.
+        is_running (bool): Is the application running.
+    """
 
-        self.event = None
-        self.is_running = True
+    def __init__(self) -> None:
+        """Initializes the application fields."""
 
-    def run(self):
+        self.surface: pygame.Surface = pygame.display.set_mode((WIDTH, HEIGHT))
+
+        self.graph: Graph = Graph()
+        self.ui: UI = UI(self, MENU)
+
+        self.event: Optional[pygame.Event] = None
+        self.is_running: bool = True
+
+    def run(self) -> None:
+        """The main application loop."""
+
         clock = pygame.time.Clock()
 
         while self.is_running:
@@ -27,17 +43,23 @@ class App:
             self.render()
 
             pygame.display.flip()
-            clock.tick(60)
+            clock.tick(FPS)
 
-    def update(self):
+    def update(self) -> None:
+        """Updates the application."""
+
         self.graph.update()
         self.ui.update(self.event)
 
-    def render(self):
+    def render(self) -> None:
+        """Render application components."""
+
         self.graph.render(self.surface)
         self.ui.render(self.surface)
 
-    def handle_events(self):
+    def handle_events(self) -> None:
+        """Handle peripheral events."""
+
         for event in pygame.event.get():
             self.event = event
 
@@ -46,11 +68,23 @@ class App:
 
             self.handle_keyboard_events(event)
 
-    def handle_keyboard_events(self, event):
+    def handle_keyboard_events(self, event: pygame.Event) -> None:
+        """Handle keyboard events.
+
+        Args:
+            event (pygame.Event): The current event.
+        """
+
         if event.type == pygame.KEYDOWN:
             self.handle_key_press(event.key)
 
-    def handle_key_press(self, key):
+    def handle_key_press(self, key: int) -> None:
+        """Handle keyboard inputs.
+
+        Args:
+            key (int): The key code.
+        """
+
         match key:
             case pygame.K_ESCAPE:
                 self.is_running = False
--- src/ui/ui.py
+++ src/ui/ui.py
@@ -1,4 +1,8 @@
+"""Summary
+"""
 import pygame
+
+from pygame.constants import SYSTEM_CURSOR_ARROW, SYSTEM_CURSOR_HAND
 
 from src.ui.button import Button
 from src.ui.slider import Slider
@@ -6,22 +10,43 @@ from src.ui.container import Container
 
 from src.utilities.utility import Utility
 
-from pygame.constants import SYSTEM_CURSOR_ARROW, SYSTEM_CURSOR_HAND
-
 
 class UI:
+    """Summary
+
+    Attributes:
+        app (TYPE): Description
+        data (TYPE): Description
+        elements (list): Description
+    """
+
     def __init__(self, app, path):
+        """Summary
+
+        Args:
+            app (TYPE): Description
+            path (TYPE): Description
+        """
+
         self.app = app
         self.data = Utility.get_json_data(path)
         self.elements = []
 
         self.initialize()
 
-    def initialize(self):
+    def initialize(self) -> None:
+        """Summary"""
+
         for elements in self.data.keys():
             self.create_elements(elements)
 
-    def create_elements(self, elements):
+    def create_elements(self, elements) -> None:
+        """Summary
+
+        Args:
+            elements (TYPE): Description
+        """
+
         match elements:
             case "buttons":
                 self.create_buttons()
@@ -34,19 +59,31 @@ class UI:
 
         self.elements.sort(key=lambda x: x.z_buffer, reverse=True)
 
-    def create_buttons(self):
+    def create_buttons(self) -> None:
+        """Summary"""
+
         for button in self.data["buttons"]:
             self.elements.append(Button(self.app, **button))
 
-    def create_containers(self):
+    def create_containers(self) -> None:
+        """Summary"""
+
         for container in self.data["containers"]:
             self.elements.append(Container(self.app, **container))
 
-    def create_sliders(self):
+    def create_sliders(self) -> None:
+        """Summary"""
+
         for slider in self.data["sliders"]:
             self.elements.append(Slider(self.app, **slider))
 
-    def handle_hover(self):
+    def handle_hover(self) -> None:
+        """Summary
+
+        Returns:
+            None: Nothing is returned
+        """
+
         is_cursor_on_element = False
 
         for element in reversed(self.elements):
@@ -62,12 +99,24 @@ class UI:
 
         pygame.mouse.set_cursor(SYSTEM_CURSOR_ARROW)
 
-    def update(self, event):
+    def update(self, event) -> None:
+        """Updates the user interface.
+
+        Args:
+            event (TYPE): Description
+        """
+
         self.handle_hover()
 
         for element in reversed(self.elements):
             element.update(event)
 
-    def render(self, surface):
+    def render(self, surface) -> None:
+        """Summary
+
+        Args:
+            surface (TYPE): Description
+        """
+
         for element in self.elements:
             element.render(surface)
